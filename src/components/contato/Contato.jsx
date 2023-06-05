365dbc294708c68f1814d70ef2c7abbe76348d04
Lucas Pablo <lucaspabloibit07@gmail.com>
2023-06-05 15:04:46 -0300

    fixed contato form and agendamento form

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -1,8 +1,31 @@
-function Contato(){
+import React from "react";
+import './Contato.css'
+
+
+
+function Contato () {
+
     return (
-        <div>
-            <h1>Contato</h1>
+        <div className="contato">
+            
+            <section>
+                <h2>Contato</h2>
+                <form action="https://api.staticforms.xyz/submit" method="post">
+                    <label>Nome</label>
+                    <input type="text" name="name" placeholder="Digite seu nome" autocomplete="off" required/>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Digite seu email" autocomplete="off" required/>
+                    <label>Mensagem</label>
+                    <textarea name="message" cols="30" rows="10" placeholder="Digite sua mensagem" required></textarea>
+                    <button type="submit">Enviar</button>
+
+                    <input type="hidden" name="accessKey" value="#"/>
+                    <input type="hidden" name="redirectTo" value="#"/>
+                </form>
+            </section>
+            
         </div>
+
     )
 }
 
